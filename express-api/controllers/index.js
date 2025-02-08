d249e1fef91a97857e828f2bd429f7005513ca5d
linguawork <mixedworldcitizen@gmail.com>
2025-02-08 12:11:35 +0100

    1:18:00, created prisma-client, controllers file, index for reexport, TODO: route functions in controller file

---
+++ express-api/controllers/index.js
@@ -0,0 +1,8 @@
+//1:15, index file will export everything from files in the folder
+const UserController = require('./user-controller')
+
+
+//своего рода реэкспорт
+module.exports = {
+    UserController
+}
---
+++ express-api/prisma/prisma-client.js
@@ -0,0 +1,8 @@
+//1:14:00
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+module.exports = {
+    prisma
+}
